7ad1ab6012dd429724d44f266d69379eaaaeab24
RayElAmal <grindingdeveloper@gmail.com>
2022-06-23 15:59:42 +0100

    changed index.js

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ let lastName = "ElAmal";
 
 let message = "Hello World, my name is:";
 
+let person = {
+    name: "Josh",
+    age: 20
+};
+
 console.log(message + ' ' + firstName + ' ' + lastName);
+
+console.log(person);
+
+let cup = {
+    design: "zigzags",
+    weight: 10,
+    material: "plastic"
+};
+
+// Dot notation --> this is one way to access and read an object
+cup.weight = 20; // in this example you migh have added some milk into the cup so now its heavier!
+
+// Bracket notation --> this is another way to access and read an object
+cup['weight'] = 10; // drank all the milk
+
+// reading an object with both ways
+console.log(cup.weight);
+console.log(cup['weight']);
